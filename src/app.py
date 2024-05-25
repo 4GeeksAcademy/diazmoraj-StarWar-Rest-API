656549ae0a6cd27cd62723b28ccd79c10a74e735
diazmoraj <103903062+diazmoraj@users.noreply.github.com>
2024-05-25 04:39:01 +0000

    Metodos de Tabla User

--- src/app.py
+++ src/app.py
@@ -48,6 +48,8 @@ def handle_hello():
     print(users_serialized)
     return jsonify({"data": "all_user"}), 200
 
+# Rutas para obtener usuarios por ID
+
 @app.route("/user/<int:id>", methods=['GET'])
 def get_single_user(id):
     single_user = User.query.get(id)
@@ -62,18 +64,23 @@ def new_user():
     body = request.get_json(silent=True)
     if body is None:
         return jsonify({"msg": "Debes enviar informacion en el body"}), 400
+    if "name" not in body:
+        return jsonify({"msg": "El campo nombre es obligatorio"}), 400
     if "email" not in body:
         return jsonify({"msg": "El campo email es obligatorio"}), 400
     if "password" not in body:
         return jsonify({"msg": "El campo password es obligatorio"}), 400
     
     new_user = User()
+    new_user.name = body["name"]
     new_user.email = body["email"]
     new_user.password = body["password"]
     db.session.add(new_user)
     db.session.commit()
 
     return jsonify({"msg": "OK"}), 200
+
+#Rutas para borrar usuarios
 
 @app.route("/user/<int:id>", methods=["DELETE"])
 def delete_user(id):
